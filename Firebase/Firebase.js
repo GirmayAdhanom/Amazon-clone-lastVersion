e9602d5aae861cd5ca413df4e2bf8929014928ce
girmayevangadi <girmayabrha@gmail.com>
2025-06-01 20:33:44 -0700

    amazon deployment

--- Firebase/Firebase.js
+++ Firebase/Firebase.js
@@ -8,12 +8,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId:import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBhGBghsnJxhlblGCkxZHFOI9WXKX1RQC0",
+  authDomain: "clone-2025-8603c.firebaseapp.com",
+  projectId: "clone-2025-8603c",
+  storageBucket: "clone-2025-8603c.firebasestorage.app",
+  messagingSenderId: "1015022463677",
+  appId: "1:1015022463677:web:ea5d6ef70cef42a461a5d4"
 };
 
 // Initialize Firebase
